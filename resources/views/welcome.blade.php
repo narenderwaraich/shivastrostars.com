dbf7c0fa1e9ec8abd8943226339f089c5915715e
singhWaraich <singh4narender@gmail.com>
2020-09-27 12:07:51 +0530

    chnge name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ShivAstroStars
                 </div>
 
                 <div class="links">
